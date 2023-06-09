d686daca43644fdf5c5fe09294c8109a0464e60a
0xcacti <0xcacti@gmail.com>
2023-06-08 23:44:08 -0400

    change to absolute paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,11 @@ const Home: NextPage = () => {
                     content="width=device-width, initial-scale=1.0"
                     name="NFTensor"
                 />
-                <link href="image/favicon.ico" rel="icon" />
-                <link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png" />
-                <link rel="manifest" href="image/site.webmanifest" />
+                <link href="/image/favicon.ico" rel="icon" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png" />
+                <link rel="manifest" href="/image/site.webmanifest" />
             </Head>
 
             <main>
